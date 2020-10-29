3cdd26c45a3ff2d402a69ef1c897ac0991052f7c
palmin-albert333 <palmin.albert@yandex.ru>
2020-10-29 16:30:01 +0300

    add terraform-2 homework

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -14,7 +14,7 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable private_key {
+variable private_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
@@ -32,4 +32,12 @@ variable service_account_key_file {
 variable app_count {
   description = "App count"
   default     = 1
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
